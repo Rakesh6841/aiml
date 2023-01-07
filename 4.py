21c1e4cd4daeee49101384b06b1dd9a0041bb54d
Rakesh Kundu <57815079+Rakesh6841@users.noreply.github.com>
2023-01-08 01:19:23 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,42 +1,73 @@
 import pandas as pd
-from pprint import pprint
-from sklearn.feature_selection import mutual_info_classif
-from collections import Counter
-
-def id3(df, target_attribute, attribute_names, default_class=None):
-    cnt=Counter(x for x in df[target_attribute])
-    if len(cnt)==1:
-        return next(iter(cnt))
-    
-    elif df.empty or (not attribute_names):
-         return default_class
-
+import math
+import numpy as np
+data = pd.read_csv("data.csv")
+features = [feat for feat in data]
+features.remove("classification")
+class Node:
+    def __init__(self):
+        self.children = []
+        self.value = ""
+        self.isLeaf = False
+        self.pred = ""
+def entropy(examples):
+    pos = 0.0
+    neg = 0.0
+    for _, row in examples.iterrows():
+        if row["classification"] == "Yes":
+            pos += 1
+        else:
+            neg += 1
+    if pos == 0.0 or neg == 0.0:
+        return 0.0
     else:
-        gainz = mutual_info_classif(df[attribute_names],df[target_attribute],discrete_features=True)
-        index_of_max=gainz.tolist().index(max(gainz))
-        best_attr=attribute_names[index_of_max]
-        tree={best_attr:{}}
-        remaining_attribute_names=[i for i in attribute_names if i!=best_attr]
-        
-        for attr_val, data_subset in df.groupby(best_attr):
-            subtree=id3(data_subset, target_attribute, remaining_attribute_names,default_class)
-            tree[best_attr][attr_val]=subtree
-        
-        return tree
-    
-
-df=pd.read_csv("p-tennis.csv")
-
-attribute_names=df.columns.tolist()
-print("List of attribut name")
-
-attribute_names.remove("PlayTennis")
-
-for colname in df.select_dtypes("object"):
-    df[colname], _ = df[colname].factorize()
-    
-print(df)
-
-tree= id3(df,"PlayTennis", attribute_names)
-print("The tree structure")
-pprint(tree)
+        p = pos / (pos + neg)
+        n = neg / (pos + neg)
+        return -(p * math.log(p, 2) + n * math.log(n, 2))
+def info_gain(examples, attr):
+    uniq = np.unique(examples[attr])
+    gain = entropy(examples)
+    for u in uniq:
+        subdata = examples[examples[attr] == u]
+        sub_e = entropy(subdata)
+        gain -= (float(len(subdata)) / float(len(examples))) * sub_e
+    return gain
+def ID3(examples, attrs):
+    root = Node()
+    max_gain = 0
+    max_feat = ""
+    for feature in attrs:
+        gain = info_gain(examples, feature)
+        if gain > max_gain:
+            max_gain = gain
+            max_feat = feature
+    root.value = max_feat
+    uniq = np.unique(examples[max_feat])
+    for u in uniq:
+        subdata = examples[examples[max_feat] == u]
+        if entropy(subdata) == 0.0:
+            newNode = Node()
+            newNode.isLeaf = True
+            newNode.value = u
+            newNode.pred = np.unique(subdata["classification"])
+            root.children.append(newNode)
+        else:
+            dummyNode = Node()
+            dummyNode.value = u
+            new_attrs = attrs.copy()
+            new_attrs.remove(max_feat)
+            child = ID3(subdata, new_attrs)
+            dummyNode.children.append(child)
+            root.children.append(dummyNode)
+    return root
+def printTree(root: Node, depth=0):
+    for i in range(depth):
+        print("\t", end="")
+    print(root.value, end="")
+    if root.isLeaf:
+        print(" -> ", root.pred)
+    print()
+    for child in root.children:
+        printTree(child, depth + 1)
+root = ID3(data, features)
+printTree(root)
