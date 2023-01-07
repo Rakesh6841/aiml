d76101b65620918b1e648c8014f9b967196db9fc
Rakesh Kundu <57815079+Rakesh6841@users.noreply.github.com>
2023-01-07 18:34:12 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -4,7 +4,7 @@ with open('dataset.csv') as f:
   csv_file = csv.reader(f)
   data = list(csv_file)
 
-  specific = data[1][:-1]
+  specific = data[0][:-1]
   general = [['?' for i in range(len(specific))] for j in range(len(specific))]
   for i in data:
     if i[-1] == 'Yes':
